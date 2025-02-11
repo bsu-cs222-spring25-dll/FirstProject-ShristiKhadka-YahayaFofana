88f92e8e8a3cfbc22f09c0a17d8bced70c5843f6
Yahaya <yahyafofana123@gmail.com>
2025-02-11 11:00:08 -0500

    Last Commit -1 : 2/11/2025

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -3,6 +3,9 @@ package edu.bsu.cs;
 //Interacts with the User and Fetches wikipedia data
 
 public class Main {
+
+
+
     public static void main(String[] args) {
         ConsoleView view = new ConsoleView();
         WikipediaController controller = new WikipediaController(view);
